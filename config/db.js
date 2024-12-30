604689ba75f1d3db3c10debf13478a4135e8e4f2
unknown <premrathodd02@gmail.com>
2024-12-30 23:31:57 +0530

    Tenth Commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Sample')
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error('MongoDB connection error:', err.message);
+        process.exit(1); // Exit process with failure
+    }
+};
 
 module.exports = connectDB;
