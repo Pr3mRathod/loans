f4bb5b75273ba78e1e44152a12a479899b919c5c
unknown <premrathodd02@gmail.com>
2024-12-30 23:28:52 +0530

    Nineth Commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/your-db')
+mongoose.connect('mongodb://localhost:27017/Sample')
   .then(() => {
     console.log('Connected to MongoDB');
   })
